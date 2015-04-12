e3023227302ef751270792a9b8d2c85da96d7df0
Nancy McNamara <nmcnama1@nd.edu>
2015-04-12 13:34:20 -0400

    additions to Pipe class & Score class

---
+++ Score.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#ifndef SCORE_H
+#define SCORE_H
+class Score {
+	public:
+		void addScore;		//increments score
+		int getScore;		//returns score
+	private:
+		int score;
+}
+
+#endif
