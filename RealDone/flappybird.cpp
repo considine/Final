474610f23aa786c5d4213277bfdeb8a24f885a82
John Patrick Considine <jconsidi@fitz68.helios.nd.edu>
2015-04-21 16:56:30 -0400

    Here we go boys

---
+++ RealDone/flappybird.cpp
@@ -0,0 +1,126 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include "bird.h"
+using namespace std;
+
+int SCREEN_HEIGHT = 450;
+int SCREEN_WIDTH = 350;
+bool init();
+SDL_Surface* loadSurface( string path );
+bool loadMedia();
+void close();
+enum KeyPressSurfaces
+{
+	KEY_PRESS_SURFACE_DEFAULT,
+	KEY_PRESS_SURFACE_UP,
+	KEY_PRESS_SURFACE_TOTAL,
+};
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* flappybird = NULL;
+SDL_Surface* gXOut = NULL;
+SDL_Surface* gKeyPressSurfaces[ KEY_PRESS_SURFACE_TOTAL ];
+SDL_Surface* gCurrentSurface = NULL;
+SDL_Surface* gStretchedSurface = NULL;
+
+
+bool init() {
+	bool success = true;
+	if ( SDL_Init( SDL_INIT_VIDEO ) < 0)
+	{
+		cout<<"Could not initialize, "<<endl;
+		success = false;
+	}
+	else 
+	{
+		gWindow = SDL_CreateWindow( "Flappy bird", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if ( gWindow == NULL ) {
+			cout <<"Could not create window"<<endl;
+			success = false;
+		}
+		else gScreenSurface = SDL_GetWindowSurface( gWindow);
+	}
+	return success;
+}
+
+bool loadMedia() // the picture on the screen
+{
+	bool success = true;
+	gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ] = loadSurface( "background.bmp");
+	
+	flappybird = loadSurface("bird.bmp");
+
+	
+	gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] = loadSurface("bird.bmp");
+	if (flappybird == NULL) return false;
+	return true;
+}
+
+void close () {
+	SDL_FreeSurface (flappybird);
+	flappybird = NULL;
+	
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	SDL_Quit();
+}
+
+SDL_Surface* loadSurface (string path) {
+	SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
+	if (loadedSurface == NULL ) cout <<"UNABLE TO LOAD SURFACE"<<path<<endl;
+	return loadedSurface;
+} 
+
+
+int main ( int argc, char* args[] ) {
+	bird jack();
+	init();
+	loadMedia();
+	bool quit = false;
+	SDL_Event e;
+	int i =0;
+
+	SDL_Rect stretchRect;
+        stretchRect.x =SCREEN_WIDTH/2;
+        stretchRect.y =SCREEN_HEIGHT/2;
+        stretchRect.w = 50;
+        stretchRect.h = 50;
+
+	while (!quit) {
+				//first redraw background
+				i++;
+                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
+                                SDL_BlitSurface( gCurrentSurface, NULL, gScreenSurface, NULL);
+                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
+                                SDL_BlitScaled( gCurrentSurface, NULL, gScreenSurface, &stretchRect);
+				if (i>8) {
+					stretchRect.y++;
+					i=0;
+				}
+		while ( SDL_PollEvent (&e )!= 0) {
+			if (e.type ==SDL_QUIT) quit = true;
+
+			else if( e.type == SDL_KEYDOWN){	
+				switch(e.key.keysym.sym)
+				{
+					case SDLK_UP:
+					stretchRect.y-=50;
+							
+					break;
+					case SDLK_ESCAPE:
+					quit = true;
+					default:
+					gCurrentSurface = gKeyPressSurfaces [KEY_PRESS_SURFACE_DEFAULT ];
+					SDL_BlitSurface( gCurrentSurface, NULL, gScreenSurface, NULL);
+					break;
+				}
+			}
+		}
+		
+		SDL_UpdateWindowSurface(gWindow);
+	}	
+	close();
+	return 0;
+}
+
---
+++ RealDone/graphics.h
@@ -0,0 +1,46 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include <SDL2/SDL.h>  //include the image library
+#include <string>
+#include "bird.h"
+#include "pipes.h"
+using namespace std;
+class graphics {
+	private:
+		int SCREEN_HEIGHT;
+		int SCREEN_WIDTH;
+		SDL_Window* gWindow; // the graphics window
+		SDL_Surface* gScreenSurface; //The screen surface
+		SDL_Surface* whichSurface[8]; //array of five different surfaces which might be loaded
+		SDL_Surface* birdA[7]; //array of five different surfaces which might be loaded
+		SDL_Surface* background;
+		SDL_Renderer* gRenderer;
+		SDL_Event e; //for quitting etc	public:
+		int score; //how many pipes have been passed
+		bool QUIT1;  //force quit
+		bool ufo; //to go into UFO mode
+		bool Invisible;  //these variables have fun effects ;)
+		bool invincible; 
+	public:
+		graphics(int = 450, int = 350); //constructor which sets the screen height and width
+		~graphics();
+		bird jack;
+		pipes bar;
+		void init(); //initializes window etc, return boolean
+		void loadMedia(); //takes an image and loads it
+		SDL_Surface* loadSurface(string path);
+		void blitSurface(SDL_Surface*); //blits the background image
+		void animate();
+		void blitbird(int, int, int); //takes length and width and draws bird		
+		void blitground(int);
+		void checkCrash(int); // sees if the bird has crashed
+		void menu(); //displays the menu
+		void initialFreeze();
+		void drawSky(); //draw the top rectangle
+		void drawPipes(); //draws the pipes, takes which pipe 
+		void lose();
+		void QUIT(); //quits
+		void UFO(); //turns on UFO mode
+		void off(); //turns cheats off
+};
+#endif
