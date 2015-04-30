11fb58640f70e8803792854b7eda3a1e84fd4f81
John Patrick Considine <jconsidi@fitz73.helios.nd.edu>
2015-04-30 14:05:31 -0400

    SHES DONE

---
+++ final/pipes.cpp
@@ -0,0 +1,90 @@
+#include "pipes.h"
+#include <iostream>
+#include <time.h>  //to initialize srand
+#include <stdlib.h> //for frand functions
+using namespace std;
+
+
+pipes::pipes()
+{
+}
+
+int pipes::randomHeight(int oldpipe) {
+	srand(time(NULL)); // initialize seed;
+	int difference = rand() % 2*change - change;  //this is where the gap may land;
+	return gety(oldpipe) + difference;
+}
+
+int pipes::getGap() 
+{
+	return gap;
+}
+
+int pipes::getx(int num) {
+	return xPositions[num];
+}
+
+int pipes::gety(int num) {
+        return yPositions[num];
+}
+
+int pipes::setx(int num, int num2) {
+        xPositions[num] = num2;
+}
+
+int pipes::sety(int num, int num2) {
+        yPositions[num] = num2;
+}
+
+void pipes::movetime(){
+	movevariable++;
+	if ((getmovevariable() %1) != 0) return;
+	int newx;
+	for (int i =0; i<3; i++) {
+	srand(time(NULL));
+		if (getx(i) < -10) {
+			setx(i, screenW*1.2);
+			sety(i, rand() % screenH/2 + screenH/4);
+		}
+		newx = getx(i);
+		setx(i, newx-speed); //iterate pipes oner pixel
+	}
+}
+
+int pipes::getmovevariable() {
+	return movevariable;
+}
+
+
+void pipes::setscreen(int width, int height) {
+	screenH = height;
+	screenW = width; // now we know
+	gap = screenH/4;
+        change = 2*gap;
+        movevariable =0;
+	speed = 1;
+	srand(time(NULL));
+        for (int i=0; i<3; i++) {
+                sety(i, screenH/2); //just to initialize
+                setx (i, screenW + i*screenW/2);
+        }
+
+}
+
+void pipes::speedup(){
+	speed++;  //speed up the moving pipes
+}
+
+void pipes::slowdown() {
+	speed--;  //slow the moving pioes
+}
+
+int pipes::getspeed() {
+	return speed;  //to interact with graphics.cpp
+}
+
+
+void pipes::changegap(int num) {
+	if (num==0) gap++;
+	else gap--;
+}
